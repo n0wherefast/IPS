5664b9a3a1cb24c0a828140c88331dbb67122c54
n0wherefast <97000096+n0wherefast@users.noreply.github.com>
2026-02-24 12:59:22 +0100

    gtm

--- app/GoogleTagManager/HeadScript.tsx
+++ app/GoogleTagManager/HeadScript.tsx
@@ -2,7 +2,7 @@ import Script from "next/script";
 
 export default function HeadScript() {
   return (
-    <Script id="gtm-script" strategy="afterInteractive">
+    <Script id="gtm-script" strategy="beforeInteractive">
       {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
